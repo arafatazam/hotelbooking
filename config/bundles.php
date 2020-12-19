eee9322b3ecfa9815b711d8d360e2c61d3dafb30
Md. Arafat Azam <arafatazam17@gmail.com>
2020-12-19 19:19:40 +0600

    Swagger added

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,8 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -6,10 +6,16 @@ use App\DTO\SignUpRequest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Swagger\Annotations as SWG;
+use Nelmio\ApiDocBundle\Annotation\Model;
 
 class AuthController extends AbstractController
 {
     /**
+     * @SWG\Response(
+     *     response=200,
+     *     @Model(type=SignUpRequest::class)
+     * )
      * @Route("/signup", name="auth.signup", methods={"POST"})
      */
     public function signUp(SignUpRequest $req): Response
